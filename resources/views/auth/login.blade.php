21f1986b6c118873f3573fd545dc8c0ea040203d
ameena shaheen <ameena-18@hotmail.com>
2021-01-12 19:31:47 +0200

    latest

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,9 +10,21 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="identify" class="col-md-4 col-form-label text-md-right">Email Or Mobile</label>
+
+                            <div class="col-md-6">
+                                <input id="identify" type="text" class="form-control @error('identify') is-invalid @enderror" name="identify" value="{{ old('identify') }}" required autocomplete="identify" >
+
+                                @error('identify')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <!--<div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{-- {{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -24,7 +36,7 @@
                                 @enderror
                             </div>
                         </div>
-                        <!--<div class="form-group row">
+                        <div class="form-group row">
                             <label for="mobile" class="col-md-4 col-form-label text-md-right">Mobile Number</label>
 
                             <div class="col-md-6">
@@ -34,7 +46,7 @@
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
+                                @enderror--}}
                             </div>
                         </div>-->
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,19 +39,19 @@
                             </div>
                         </div>
 
-                        <!--<div class="form-group row">
+                        <div class="form-group row">
                             <label for="mobile" class="col-md-4 col-form-label text-md-right">Mobile Number</label>
 
                             <div class="col-md-6">
-                                <input id="mobile" type="text" class="form-control {{--@error('mobile') is-invalid @enderror" name="mobile" value="{{ old('mobile') }}" required autocomplete="mobile">
+                                <input id="mobile" type="text" class="form-control @error('mobile') is-invalid @enderror" name="mobile" value="{{ old('mobile') }}" required autocomplete="mobile">
 
                                 @error('mobile')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror--}}
+                                @enderror
                             </div>
-                        </div>--> 
+                        </div>
 
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
